a8b552e1b22a388d423c0ccd1eb5ef23c4d58327
Nasir Ibrahim <nasir.moh.ibrahim@gmail.com>
2024-07-02 06:35:11 +0100

    published the API

--- obj/Release/net8.0/HelloWorldApi.AssemblyInfo.cs
+++ obj/Release/net8.0/HelloWorldApi.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56344818d8a4b636d7608cdb1aabfd5235ef99a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApi")]
